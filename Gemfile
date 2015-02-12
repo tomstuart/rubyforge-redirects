a4647db4c0d59ba5fd93958a9c7142256312c25a
James Adam <james@lazyatom.com>
2015-02-12 13:34:59 +0000

    Show a basic helpful 404 page if no redirect exists.
    This should let people search to find the gem/project they wanted, and
    give instruction about how to contribute a redirect if they'll like to.

--- Gemfile
+++ Gemfile
@@ -4,9 +4,11 @@ ruby '2.2.0'
 gem 'puma'
 gem 'rack'
 gem 'rack-rewrite'
+gem 'rack-contrib'
 
 group :test do
   gem 'cucumber'
   gem 'rack-test'
   gem 'rspec-expectations'
+  gem 'capybara'
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,13 @@
 require 'rack'
 require 'rack/test'
+require 'capybara/cucumber'
 
 module App
   def app
     Rack::Builder.parse_file('config.ru').first
   end
+  extend self
 end
 
 World(Rack::Test::Methods, App)
+Capybara.app = App.app
